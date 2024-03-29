006bd7dcb3b42267c1e18af13f1324b4ea1473dc
codercake <ishithaac2312@gmail.com>
2024-03-29 21:39:00 +0530

    accordian

---
+++ src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+      id  : '1',
+      question: "What is Next.js?",
+      answer:
+    "Next.js is a popular React framework that is used for building server-side rendered (SSR) and statically generated (SSG) web applications. It provides developers with several features and optimizations out of the box, making it easier to build complex React applications." ,
+    },
+    {
+      id  : '2',
+      question: "How is it different from React.js?",
+      answer:
+        "React.js is a library for building UI components, while Next.js is a framework built on top of React.js that provides additional features like SSR, SSG, routing, and API routes to simplify full-stack web development. Depending on the requirements of your project, you may choose to use React.js alone or opt for Next.js to leverage its built-in features and optimizations.",
+    },
+    {
+      id  : '3',
+      question: "Next.js with Backend",
+      answer:
+        "You can build powerful full-stack web applications with seamless client-server communication, efficient data fetching, and robust backend functionality. The choice of backend technology depends on factors such as your project requirements, familiarity with the technology stack, scalability, and performance considerations.",
+    },
+    {
+      id  : '4',
+      question: "Future of Next.js?",
+      answer:
+        "The future of Next.js looks promising, driven by its ongoing innovation, growing ecosystem, increasing adoption, support for modern development practices, and strong community involvement. As web development continues to evolve, Next.js is likely to remain a leading framework for building high-quality web applications.",
+    },
+  ];
+
+  export default data;
